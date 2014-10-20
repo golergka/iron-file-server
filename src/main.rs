b4735d4432a46e37f52c8f09cb5b514948853f13
Max Yankov <golergka@gmail.com>
2014-10-20 14:04:47 +0300

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate iron;
+
+use std::io::net::ip::Ipv4Addr;
+use iron::{Iron, Request, Response, IronResult};
+use iron::status;
+
+fn hello_world(_:&mut Request) -> IronResult<Response> {
+    Ok(Response::with(status::Ok, "Hello world!"))
+}
+
 fn main() {
-    println!("Hello, world!")
+    Iron::new(hello_world).listen(Ipv4Addr(127, 0, 0, 1), 3000)
 }
